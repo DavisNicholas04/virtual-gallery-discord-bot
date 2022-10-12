e2e4af2b1ee7cf22dee721d4b2fd23f945c4474e
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-10-11 22:52:12 -0400

    prep for json parser

--- src/virtual-gallery/main.py
+++ src/virtual-gallery/main.py
@@ -1,5 +1,6 @@
 import os
 import interactions
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,16 +12,25 @@ client = interactions.Client(token=os.environ['DISCORD_TOKEN'], intents=intent)
     description="start the interaction, which will tell the bot to message the user privately",
 )
 async def start(msg: interactions.ComponentContext):
-    await msg.send(f"starting virtual gallery with {msg.author.name} in private messages.")
-    await msg.author.send("If you're reading this it's already too late for you. "
-                          "I'm behind you and won't wait for you to turn around before I swing my axe")
-    await msg.author.send("Also, welcome to the interactive virtual reality experience. For a list of commands use the ``/help`` command")
+    await msg.send(f"starting virtual gallery with {msg.author.name} in private messages.\n"
+                    "If you're reading this it's already too late for you. "
+                    "I'm behind you and won't wait for you to turn around before I swing my axe")
+
+    await msg.author.send("Also, welcome to the interactive virtual reality experience. "
+                          "For a list of commands use the ``/help`` command")
+
     options = interactions.SelectMenu(
         custom_id="genre_select",
         options=[
-            interactions.SelectOption(custom_id="custom_id_history", label="History", value="value_History", decription="description_history"),
-            interactions.SelectOption(custom_id="custom_id_anime", label="Anime", value="anime", decription="description_anime"),
-            interactions.SelectOption(custom_id="custom_id_games", label="Games", value="games", decription="description_games")],
+            interactions.SelectOption(custom_id="custom_id_history", label="History",
+                                      value="value_History", decription="description_history"),
+
+            interactions.SelectOption(custom_id="custom_id_anime", label="Anime",
+                                      value="anime", decription="description_anime"),
+
+            interactions.SelectOption(custom_id="custom_id_games", label="Games",
+                                      value="games", decription="description_games")],
+
         placeholder="Choose your experience",
     )
     await msg.author.send(content="choose your genre", components=options)
@@ -29,5 +39,9 @@ async def start(msg: interactions.ComponentContext):
 @client.component("genre_select")
 async def select_menu_response(ctx: interactions.ComponentContext, selection):
     await ctx.send(f"You selected {selection[0]}. Great choice. {client.me.guild_id}")
+
+
+    # json.loads()
+
 
 client.start()
