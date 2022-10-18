439ff90b5c3e89127577167e8f856c88e70e3a9f
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-10-17 21:35:45 -0400

    put back

--- src/main.py
+++ src/main.py
@@ -55,38 +55,8 @@ if __name__ == "__main__":
         )
         d = await ctx.send(f"You selected {selection[0]}. Great choice!")
         await ctx.send("**you have a few options. select the one that interests you.**", components=reroll_button)
-        # await roll(ctx, selection)
-        if selection[0] == "history":
-            second_button_label = "Official Webpage"
-        else:
-            second_button_label = "Buy now!"
-        resources = get_two_recourses(selection[0])
-        reroll_button_ids_dict[f"{ctx.user}-selection"] = selection
-        i = 0
-        msgs: [interactions.Message]
-        for entity in resources:
-            i = i + 1
-            button = interactions.Button(
-                style=interactions.ButtonStyle.LINK,
-                label=f"I'm interested in {entity['title'].split()[:15]}",
-                url=' '.join(entity['title'].split()[:15])
-            )
-            button2 = interactions.Button(
-                style=interactions.ButtonStyle.LINK,
-                label=second_button_label,
-                url=entity["official_link"]
-            )
-            # reroll_button_ids_dict[f"{ctx.user}-{i}"] = \
-            await ctx.channel.send(
-                    f"Title: {entity['title']}\n"
-                    f"Date:  {entity['date']}\n"
-                    f"Description:  {' '.join(str(entity['description']).split()[:20])}. . .",
-                    files=interactions.File(
-                        entity['cv_image'],
-                        fp=get_resource_from_github(f"images/{selection[0]}", entity["cv_image"], False)
-                    ),
-                    components=[button, button2]
-                )
+        await roll(ctx, selection)
+
 
     async def roll(ctx: interactions.ComponentContext, selection):
         if selection[0] == "history":
