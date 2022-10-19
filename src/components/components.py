f1b03af2b6fd1c5659ab090ac259000a3d5d36cf
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-10-19 13:37:48 -0400

    organized components

---
+++ src/components/components.py
@@ -0,0 +1,160 @@
+import interactions
+
+# BUTTONS
+
+website_button = interactions.Button(
+    style=interactions.ButtonStyle.LINK,
+    label="go to website",
+    url="https://cs.oswego.edu/~acascen/coursework/Virtual%20Gallery.HTML"
+)
+
+end_interaction_button = interactions.Button(
+    style=interactions.ButtonStyle.DANGER,
+    label=f"END SESSION",
+    custom_id="end"
+)
+
+def create_roll_buttons(entity, second_button_label):
+    button = interactions.Button(
+        style=interactions.ButtonStyle.LINK,
+        label=f"I'm interested in {entity['title'].split()[:15]}",
+        url="https://www.cs.oswego.edu/~acascen/coursework/Virtual%20Gallery.HTML"
+    )
+    button2 = interactions.Button(
+        style=interactions.ButtonStyle.LINK,
+        label=second_button_label,
+        url=entity["official_link"]
+    )
+    return [button, button2]
+# /-S-T-A-R-T--------------------\
+# | re-roll buttons              |
+# \------------------------------/
+original_rr_button = interactions.Button(
+    style=interactions.ButtonStyle.PRIMARY,  # Original re-roll button
+    label="reroll",
+    custom_id="reroll"
+)
+
+disabled_rr_button = interactions.Button(
+    style=interactions.ButtonStyle.PRIMARY,  # re-roll button - disabled
+    label="reroll",
+    custom_id="reroll",
+    disabled=True
+)
+
+success_rr_button = interactions.Button(
+    style=interactions.ButtonStyle.SUCCESS,  # re-roll button - success
+    label="reroll",
+    custom_id="reroll"
+)
+
+change_genre_button = interactions.Button(
+    style=interactions.ButtonStyle.SECONDARY,  # change genre button
+    label="change genre",
+    custom_id="change_genre"
+)
+
+enabled_button_group = [original_rr_button, change_genre_button, end_interaction_button]
+disabled_button_group = [disabled_rr_button, change_genre_button, end_interaction_button]
+success_button_group = [success_rr_button, change_genre_button, end_interaction_button]
+# /------------------------E-N-D-\
+# | re-roll buttons              |
+# \------------------------------/
+
+# SELECT MENU
+# /-S-T-A-R-T--------------------\
+# | genre select select menu     |
+# \------------------------------/
+history_select = interactions.SelectOption(
+    custom_id="custom_id_history",
+    label="History",
+    value="history",
+    decription="description_history"
+)
+
+anime_select = interactions.SelectOption(
+    custom_id="custom_id_anime",
+    label="Anime",
+    value="anime",
+    decription="description_anime"
+)
+
+games_select = interactions.SelectOption(
+    custom_id="custom_id_games",
+    label="Games",
+    value="games",
+    decription="description_games"
+)
+
+genre_select = interactions.SelectMenu(
+    custom_id="genre_select",
+    placeholder="select",
+    options=[
+        history_select,
+        anime_select,
+        games_select,
+    ]
+)
+# /------------------------E-N-D-\
+# | genre select select menu     |
+# \------------------------------/
+
+# MODAL
+# /-S-T-A-R-T--------------------\
+# | survey modal                 |
+# \------------------------------/
+username_prompt = interactions.TextInput(
+    label="username (if you would like a follow-up)",
+    style=interactions.TextStyleType.SHORT,
+    placeholder="username",
+    default="anonymous",
+    max_length=32,
+    custom_id="input1"
+)
+
+experience_prompt = interactions.TextInput(
+    label="Did you enjoy your experience with us today?",
+    style=interactions.TextStyleType.SHORT,
+    placeholder="yes or no",
+    max_length=3,
+    custom_id="input1"
+)
+
+experience_exp_prompt = interactions.TextInput(
+    label="If no, why not?",
+    style=interactions.TextStyleType.PARAGRAPH,
+    custom_id="input2",
+    max_length=400,
+    required=False
+)
+
+improve_us_prompt = interactions.TextInput(
+    label="could we improve your experience? How?",
+    style=interactions.TextStyleType.PARAGRAPH,
+    max_length=400,
+    custom_id="input3",
+)
+
+report_bugs_prompt = interactions.TextInput(
+    label="Bugs? report them here.",
+    style=interactions.TextStyleType.PARAGRAPH,
+    custom_id="input4",
+    max_length=700,
+    required=False
+)
+
+survey = interactions.Modal(
+    title="Survey",
+    custom_id="survey",
+    components=[
+        username_prompt,
+        experience_prompt,
+        experience_exp_prompt,
+        improve_us_prompt,
+        report_bugs_prompt,
+    ]
+)
+# /------------------------E-N-D-\
+# | survey modal                 |
+# \------------------------------/
+
--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import time
 
 import interactions
 from dotenv import load_dotenv
+import src.components.components as cpnts
 from src.controller.controller import get_resource_from_github, get_two_recourses
-# from src.service.helper_service import multi_component
 
 reroll_button_ids_dict = {}
 
@@ -14,121 +14,66 @@ if __name__ == "__main__":
     intent = interactions.Intents.DEFAULT
     client = interactions.Client(token=os.environ['DISCORD_TOKEN'], intents=intent)
 
+
     @client.command(
         name="start",
         description="start the interaction, which will tell the bot to message the user privately",
     )
     async def start(ctx: interactions.ComponentContext):
         await ctx.send("Thanks! I sent you a private message so lets get started!")
-        website_button = interactions.Button(
-            style=interactions.ButtonStyle.LINK,
-            label="go to website",
-            url="https://cs.oswego.edu/~acascen/coursework/Virtual%20Gallery.HTML"
-        )
         await ctx.user.send("Welcome to the interactive virtual gallery experience. "
-                       "For a list of commands use the ``/help`` command.\n"
-                       "You can also go straight to our website here.", components=website_button)
+                            "For a list of commands use the ``/help`` command.\n"
+                            "You can also go straight to our website here.",
+                            components=cpnts.website_button)
         await selection_menu(ctx)
 
 
     async def selection_menu(ctx: interactions.ComponentContext):
-        options = interactions.SelectMenu(
-            custom_id="genre_select",
-            options=[
-                interactions.SelectOption(custom_id="custom_id_history", label="History",
-                                          value="history", decription="description_history"),
+        await ctx.user.send(content="select one of the following options", components=cpnts.genre_select)
 
-                interactions.SelectOption(custom_id="custom_id_anime", label="Anime",
-                                          value="anime", decription="description_anime"),
-
-                interactions.SelectOption(custom_id="custom_id_games", label="Games",
-                                          value="games", decription="description_games")],
-
-            placeholder="select",
-        )
-        await ctx.user.send(content="select one of the following options", components=options)
 
     @client.component("genre_select")
     async def select_menu_response(ctx: interactions.ComponentContext, selection):
-        new_options_buttons = [
-            interactions.Button(
-                style=interactions.ButtonStyle.PRIMARY,
-                label="reroll",
-                custom_id="reroll"
-            ),
-            interactions.Button(
-                style=interactions.ButtonStyle.SECONDARY,
-                label="change genre",
-                custom_id="change_genre"
-            )
-        ]
+        new_options_buttons = cpnts.enabled_button_group
         await ctx.disable_all_components()
         await ctx.send(f"You selected {selection[0]}. Great choice!")
         # await   ctx.author.send(f"You selected {selection[0]}. Great choice!")
         await ctx.send("**you have a few options. select the one that interests you.**", components=new_options_buttons)
         await roll(ctx, selection)
 
+
     async def roll(ctx: interactions.ComponentContext, selection):
+        # choose second button label base on user selection
         if selection[0] == "history":
             second_button_label = "Official Webpage"
         else:
             second_button_label = "Buy now!"
+
         resources = get_two_recourses(selection[0])
         reroll_button_ids_dict[f"{ctx.user}-selection"] = selection
-        i = 0
-        # msgs: [interactions.Message]
-        for entity in resources:
-            i = i + 1
-            button = interactions.Button(
-                style=interactions.ButtonStyle.LINK,
-                label=f"I'm interested in {entity['title'].split()[:15]}",
-                url="https://www.cs.oswego.edu/~acascen/coursework/Virtual%20Gallery.HTML"
-            )
-            button2 = interactions.Button(
-                style=interactions.ButtonStyle.LINK,
-                label=second_button_label,
-                url=entity["official_link"]
-            )
+
+        for entity, i in resources, range(resources):
+            rolls = cpnts.create_roll_buttons(entity, second_button_label)
             reroll_button_ids_dict[f"{ctx.user}-{i}"] = \
                 await ctx.user.send(
-                    f"Title: {entity['title']}\n"
+                    f"Title:  {entity['title']}\n"
                     f"Date:  {entity['date']}\n"
                     f"Description:  {' '.join(str(entity['description']).split()[:20])}. . .",
-                    files=interactions.File(
-                        entity['cv_image'],
-                        fp=get_resource_from_github(f"images/{selection[0]}", entity["cv_image"], False)
-                    ),
-                    components=[button, button2],
+                    files=get_image(entity, selection),
+                    components=rolls,
                 )
+
+
+    def get_image(entity, selection):
+        interactions.File(
+            entity['cv_image'],
+            fp=get_resource_from_github(f"images/{selection[0]}", entity["cv_image"], False)
+        )
+
 
     @client.component("reroll")
     async def reroll(ctx: interactions.ComponentContext):
-        new_options_buttons = [
-            interactions.Button(
-                style=interactions.ButtonStyle.PRIMARY,
-                label="reroll",
-                custom_id="reroll",
-                disabled=True
-            ),
-            interactions.Button(
-                style=interactions.ButtonStyle.SECONDARY,
-                label="change genre",
-                custom_id="change_genre"
-            )
-        ]
-        new_options_buttons2 = [
-            interactions.Button(
-                style=interactions.ButtonStyle.SUCCESS,
-                label="reroll",
-                custom_id="reroll"
-            ),
-            interactions.Button(
-                style=interactions.ButtonStyle.SECONDARY,
-                label="change genre",
-                custom_id="change_genre"
-            )
-        ]
-        await ctx.message.edit(components=new_options_buttons)
+        await ctx.message.edit(components=cpnts.disabled_button_group)
         await ctx.defer(edit_origin=True)
         await delete_rolls(ctx)
         selection = reroll_button_ids_dict.get(f"{ctx.user}-selection")
@@ -137,8 +82,9 @@ if __name__ == "__main__":
         reroll_button_ids_dict.pop(f"{ctx.user}-{2}")
         await roll(ctx, selection)
         threading.Thread(
-            target=await send_edited_button(ctx,new_options_buttons2)
+            target=await send_edited_button(ctx, cpnts.success_button_group)
         )
+
 
     async def send_edited_button(ctx, new_options_buttons2):
         time.sleep(4)
@@ -151,58 +97,31 @@ if __name__ == "__main__":
         await ctx.message.delete()
         await selection_menu(ctx)
 
-    async def delete_rolls(ctx):
-        msg = await ctx.message.get_from_url(reroll_button_ids_dict.get(f"{ctx.user}-{1}").url, client=client._http)
-        await msg.delete()
-        msg = await ctx.message.get_from_url(reroll_button_ids_dict.get(f"{ctx.user}-{2}").url, client=client._http)
-        await msg.delete()
 
-    @client.command(
-        name="end",
-        description="ends the interaction, opens an optional survey",
-    )
-    async def end(ctx: interactions.CommandContext):
-        survey = interactions.Modal(
-            title="Survey",
-            custom_id="survey",
-            components=[
-                interactions.TextInput(
-                    label="username (if you would like a follow-up)",
-                    style=interactions.TextStyleType.SHORT,
-                    placeholder="username",
-                    default="anonymous",
-                    max_length=32,
-                    custom_id="input1"
-                ),
-                interactions.TextInput(
-                    label="Did you enjoy your experience with us today?",
-                    style=interactions.TextStyleType.SHORT,
-                    placeholder="yes or no",
-                    max_length=3,
-                    custom_id="input1"
-                ),
-                interactions.TextInput(
-                    label="If no, why not?",
-                    style=interactions.TextStyleType.PARAGRAPH,
-                    custom_id="input2",
-                    required=False
+    async def delete_rolls(ctx: interactions.ComponentContext):
+        """
+        find saved roll messages by user_id key saved in dict and delete
 
-                ),
-                interactions.TextInput(
-                    label="How could we improve your experience?",
-                    style=interactions.TextStyleType.PARAGRAPH,
-                    custom_id="input3",
-
-                ),
-                interactions.TextInput(
-                    label="Bugs? report them here.",
-                    style=interactions.TextStyleType.PARAGRAPH,
-                    custom_id="input4",
-                    required=False
-                ),
-            ]
+        :param ctx: component context sent in the previous interactions.Client.component function
+        :return: null
+        """
+        msg = await ctx.message.get_from_url(
+            reroll_button_ids_dict.get(f"{ctx.user}-{1}").url,
+            client=client._http
         )
-        await ctx.popup(survey)
+        await msg.delete()
+
+        msg = await ctx.message.get_from_url(
+            reroll_button_ids_dict.get(f"{ctx.user}-{2}").url,
+            client=client._http
+        )
+        await msg.delete()
+
+
+    @client.component("end")
+    async def end(ctx: interactions.CommandContext):
+        await ctx.popup(cpnts.survey)
+
 
     @client.modal("survey")
     async def change_genre(ctx: interactions.CommandContext, input1, input2, input3, input4):
@@ -218,5 +137,6 @@ if __name__ == "__main__":
             **{input4}**
             """)
         await ctx.send("Thank you for filling out the survey. Join us again!")
+
 
     client.start()
