5a4f6eb256cc6d3725f40c74bec9c4ad18a5f7e4
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-11-01 23:30:33 -0400

    changed var to let

--- docs/scripts/gallery1.js
+++ docs/scripts/gallery1.js
@@ -1,16 +1,16 @@
-var myIndex = 0;
-var images = document.getElementsByClassName("myslides");
+let myIndex = 0;
+let images = document.getElementsByClassName("myslides");
 setInterval(carousel, 2000); // Change image every 2 seconds
 
 function carousel() {
-    images[myIndex].style.display = "none";  
+    images[myIndex].style.display = "none";
 
     myIndex++;
     if (myIndex >= images.length) {
-      myIndex = 0;
+        myIndex = 0;
     }
 
     console.log(myIndex, images.length);
-    
-    images[myIndex].style.display = "block";  
+
+    images[myIndex].style.display = "block";
 }
