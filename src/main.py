8c1d4a1eb2b174595cd8e6e917d1097a3f6c52ef
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-11-04 15:04:40 -0400

    added comments

--- src/main.py
+++ src/main.py
@@ -7,7 +7,15 @@ if __name__ == "__main__":
     load_dotenv()
     intent = interactions.Intents.DEFAULT
     client = interactions.Client(token=os.environ['DISCORD_TOKEN'], intents=intent)
+
+    # Load extension files to load in appropriate listeners and their corresponding logic.
+
+    # Load listener for slash commands.
     client.load("src.extensions.command_extensions")
+
+    # load lister for interactive components (buttons & menus)
     client.load("src.extensions.component_extensions")
+
+    # load modal listener
     client.load("src.extensions.modal_extensions")
     client.start()
